858c19555669f3bc46ed3a1749f93f105be74470
Maria <Salvito1956@gmail.com>
2023-03-15 10:26:21 -0300

    atualização dois pontom zero

--- vetor.js
+++ vetor.js
@@ -8,8 +8,21 @@ function inserir(){
     res.innerHTML = valor_cidade 
 }
 
+function insert_inicio(){
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.unshift(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade 
+}
+
 function excluir(){
-    valor_cidade.pop(cidade)
+    valor_cidade.pop()
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade 
+}
+
+function excluir_inicio(){
+    valor_cidade.shift()
     console.log(valor_cidade)
     res.innerHTML = valor_cidade 
 }
