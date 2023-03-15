b83fb702d43f277104182c4355863e3184bb2cf0
Maria <Salvito1956@gmail.com>
2023-03-15 09:36:27 -0300

    atualizado

--- vetor.js
+++ vetor.js
@@ -1,11 +1,15 @@
 let valor_cidade = [] //variavel global do tipo array
+let  res = document.getElementById('res')
 
 function inserir(){
     let cidade = document.getElementById('cidade').value
     valor_cidade.push(cidade)
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade 
 }
+
 function excluir(){
     valor_cidade.pop(cidade)
     console.log(valor_cidade)
+    res.innerHTML = valor_cidade 
 }
